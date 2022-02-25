40d2d457f2e80c0dba04ee91779c9c5cb37c74b3
Puneethraj0811 <79778679+Puneethraj0811@users.noreply.github.com>
2022-02-25 16:36:42 +0530

    Add files via upload

---
+++ ShellScript/5a.sh
@@ -0,0 +1,11 @@
+#Write a shell script that accepts filename as argument and display its creation time if file
+#exist and if does not send output error message.
+
+file1=$1
+if [ -f $file1 ]
+then
+	set -- `ls -l $file1`
+	echo "Modified time of $file1 is $8"
+else
+	echo "$file1 not found"
+fi
---
+++ ShellScript/6b.sh
@@ -0,0 +1,13 @@
+#Write a shell script to list all the files in a directory whose filename is at least 10 characters.
+#(use expr command to check the length).
+
+for i in `ls`
+do 	
+	val=`expr length $i`
+	if  [ $val -ge 10 ]
+	then 
+		echo "$i"
+	else
+		echo "filename $i is not greater than 10 characters"
+	fi
+done
