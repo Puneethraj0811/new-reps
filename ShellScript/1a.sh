c964055c6484e6604e0e95069866d8bc99a0006d
Puneethraj K <79778679+Puneethraj0811@users.noreply.github.com>
2022-05-16 21:34:30 +0530

    Update 1a.sh

--- ShellScript/1a.sh
+++ ShellScript/1a.sh
@@ -1,4 +1,4 @@
-#1a. Write a shell script that takes a valid directory name as a argument recursively descend all the sub-directors, find the maximum length of 
+#Write a shell script that takes a valid directory name as a argument recursively descend all the sub-directors, find the maximum length of 
 #any file in that hierarchy and writ the maximum value to the standard output.
 
 if [ $# -eq 1 ]
