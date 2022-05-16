e31f721c47e13e689efe6a9ae6dd46fc866dc6fb
Puneethraj K <79778679+Puneethraj0811@users.noreply.github.com>
2022-05-16 21:36:38 +0530

    Update 3b.sh

--- ShellScript/3b.sh
+++ ShellScript/3b.sh
@@ -1,3 +1,8 @@
+#Write a shell script to implement terminal locking (Similar to the lock command). It should prompt for the user for a password. After accepting 
+#the password entered by the user, it must prompt again for the matching password as confirmation and if match occurs, it must lock the keyword until 
+#a matching password is entered again by the user. Note the Script must be written to disregard BREAK, control-D. No time limit need be implemented for 
+$the lock duration.
+
 echo Enter password
 stty -echo
 read p1
@@ -27,4 +32,3 @@ do
 	read p3
 done
 echo Terminal unlocked
-
