5e6aa606e6074429999d4b09088a8f671e485446
Puneethraj K <79778679+Puneethraj0811@users.noreply.github.com>
2022-05-16 21:33:05 +0530

    Update 3a.sh

--- ShellScript/3a.sh
+++ ShellScript/3a.sh
@@ -1,5 +1,4 @@
-#Create a script file called file properties that reads a filename entered and outputs it
-#properties.
+#Create a script file called file properties that reads a filename entered and outputs it properties.
 
 if [ -e $1 ]
 then 
