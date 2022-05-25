131e0ce2bb772b4d732c26e799696bc7dcfc283c
Puneethraj K <79778679+Puneethraj0811@users.noreply.github.com>
2022-05-25 20:31:31 +0530

    Update 4b.sh

--- ShellScript/4b.sh
+++ ShellScript/4b.sh
@@ -3,22 +3,31 @@
 #In either case, the starting directory as well as its subdirectories at all levels must be searched. The script need not include error checking.
 
 if [ $# -ge 1 ]
-then	
+then
 	if [ $# -eq 2 ]
 	then
-		chd=$2
+		sd=$2
 	else
-		chd=`cd .`
+		sd="."
 	fi
-	file=`find $chd -iname $1`
-	if [ `echo $file | wc -c` -gt 1 ]
-	then 
-		number=`ls -inum $file | grep -o ^[0-9]*`
-		cd $chd
-		find $chd -inum $number
+	
+	file=$1
+	if [ -f $file ]
+	then
+		
+		set -- `ls -i $file`
+		inode=$1
+		tfiles=`find $sd -inum $inode | wc -l`
+		if [ $tfiles -eq 1 ]
+		then
+			echo "$file has no link files"
+		else
+			find $sd -inum $inode
+			echo "The inode number for $tfiles files is $inode"
+		fi
 	else
-		echo No such file
+		echo "Give valid file"
 	fi
 else
-	echo Provide filename
+	echo "Give atleast one arguement"
 fi
