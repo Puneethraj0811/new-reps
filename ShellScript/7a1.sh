94b796b8c3f441a36b61a048cb9c887bb7400e7b
Puneethraj K <79778679+Puneethraj0811@users.noreply.github.com>
2022-05-29 21:00:50 +0530

    Add files via upload

---
+++ ShellScript/7a1.sh
@@ -0,0 +1,19 @@
+user=`whoami`
+set -- `who | grep $user` 
+hr=$4
+echo User Login time $hr
+set -- `echo $hr | tr ":" " "`
+h=$1
+m=$2
+if [ $h -ge 4 ] && [ $h -lt 12 ]
+then
+	echo Good morning Mr./Mrs. $user
+elif [ $h -ge 12 ] && [ $h -lt 16 ]
+then
+	echo Good afternoon Mr./Mrs. $user
+elif [ $h -ge 16 ] && [ $h -lt 19 ]
+then
+	echo Good evening Mr./Mrs. $user
+else
+	echo Good night Mr./Mrs. $user
+fi
