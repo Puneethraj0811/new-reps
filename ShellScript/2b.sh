5e08e1704471b34c9593173f1ac3c8c8f29b5090
Puneethraj K <79778679+Puneethraj0811@users.noreply.github.com>
2022-05-30 21:38:33 +0530

    Update 2b.sh

--- ShellScript/2b.sh
+++ ShellScript/2b.sh
@@ -1,19 +1,20 @@
 #Write a shell script which accepts valid log-in names as arguments and prints their corresponding home directories, if no arguments are specified, 
 #print a suitable error message.
 
-if [ $# -eq 1 ]
+if [ $# -ne 0 ]
 then
-	grep $1 /etc/passwd>ud
-	if [ $? -eq 0 ]
-	then
-		echo "user exit"
-		h=`cut -d ":" -f 6 ud`
-		echo Home directory of $1 is $h
-	else
-		echo "user not found"
-	fi 
+	for i in $* 
+	do
+		grep $1 /etc/passwd>ud
+		if [ $? -eq 0 ]
+		then
+			echo "user exit"
+			h=`cut -d ":" -f 6 ud`
+			echo Home directory of $1 is $h
+		else
+			echo "user not found"
+		fi 
+	done
 else
 	echo "enter a login\user name"
 fi
-
-
