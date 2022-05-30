9801533284bd0c525abe96b00e5bd061bbc01a83
Puneethraj K <79778679+Puneethraj0811@users.noreply.github.com>
2022-05-30 21:43:25 +0530

    Update 5a.sh

--- ShellScript/5a.sh
+++ ShellScript/5a.sh
@@ -1,10 +1,15 @@
 #Write a shell script that accepts filename as argument and display its creation time if fileexist and if does not send output error message.
 
-file1=$1
-if [ -f $file1 ]
+if [ $# -eq 1 ]
 then
-	set -- `ls -l $file1`
-	echo "Modified time of $file1 is $8"
+	file1=$1
+	if [ -f $file1 ]
+	then
+		set -- `ls -l $file1`
+		echo "$file1 created at $6 $7 $8"
+	else
+		echo "$file1 not found"
+	fi
 else
-	echo "$file1 not found"
+	echo Enter filename
 fi
